9440b77fd926755d032b8d26c5cddcd1ce81f46d
Patt Sookmark <pijiya@sookmark.com>
2022-11-13 21:57:36 -0700

    Add querySelector to main

---
+++ main.js
@@ -0,0 +1,19 @@
+//Add variables/querySlector
+var a1Btn = document.querySelector('#A1')
+var a2Btn = document.querySelector('#A2')
+var a3Btn = document.querySelector('#A3')
+var b1Btn = document.querySelector('#B1')
+var b2Btn = document.querySelector('#B2')
+var b3Btn = document.querySelector('#B3')
+var c1Btn = document.querySelector('#C1')
+var c2Btn = document.querySelector('#C2')
+var c3Btn = document.querySelector('#C3')
+
+//Add event listener
+a1Btn = document.addEventListener('click', fnName)
+
+
+
+//Add functions
+// when click token of p1 show up
+// alternate token 
